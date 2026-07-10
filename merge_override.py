2aae475319d05b01849983e744bb180e46081e7f
cdlongbow <1730218335@qq.com>
2026-07-10 23:09:25 +0800

    Update merge_override.py

--- merge_override.py
+++ merge_override.py
@@ -11,7 +11,7 @@ custom_rules = [
     "rules.unshift(\"DOMAIN-SUFFIX,c97k.com,DIRECT\");",
     "rules.unshift(\"DOMAIN-SUFFIX,javdb573.com,DIRECT\");",
     "rules.unshift(\"DOMAIN-SUFFIX,javdb.com,US美国\");",
-    "rules.unshift(\"DOMAIN-SUFFIX,15.204.105.50:25461,SG新加坡\");",
+    "rules.unshift(\"IP-CIDR,15.204.105.50/32,SG新加坡,no-resolve\");",
     "rules.unshift(\"DOMAIN-KEYWORD,dmm,JP日本\");",
     "rules.unshift(\"DOMAIN-KEYWORD,mgstage,JP日本\");",
     "rules.unshift(\"DOMAIN-SUFFIX,amazon.co.jp,JP日本\");",
